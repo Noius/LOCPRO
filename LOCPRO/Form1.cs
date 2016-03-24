1928adb40ed98233e11a4ed952c9dc3dcd9c124e
Noius <pikaben24@gmail.com>
2016-03-24 17:10:57 +0100

    connection
    connecion à la bd

--- LOCPRO/Form1.cs
+++ LOCPRO/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace LOCPRO
 {
@@ -14,6 +16,10 @@ namespace LOCPRO
     public partial class Form1 : Form
     {
         MdiClient ctlMDI;
+        private MySqlConnection myConnection;
+        private bool connopen = false;
+        private bool errgrave = false;
+        private bool chargement = false;
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +32,7 @@ namespace LOCPRO
 
         private void co_click(object sender, EventArgs e)
         {
-
+            seconnecter();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -42,8 +48,55 @@ namespace LOCPRO
                 {
                 }
             }
-
-
+        }
+        public bool Connopen
+        {
+            get { return connopen; }
+            set { connopen = value; }
+        }
+        public bool Errgrave
+        {
+            get { return errgrave; }
+            set { errgrave = value; }
+        }
+        public bool Chargement
+        {
+            get { return chargement; }
+            set { chargement = value; }
+        }
+        public void seconnecter()
+        {
+            string myConnectionString = "Database=bdd_loc;Data Source=localhost;User Id=root;";
+            myConnection = new MySqlConnection(myConnectionString);
+            try // tentative
+            {
+                myConnection.Open();
+                MessageBox.Show("Connection réussie");
+                connopen = true;
+            }
+            catch (Exception err)// gestion des erreurs
+            {
+                MessageBox.Show("Erreur ouverture bdd : " + err, "PBS connection", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+                connopen = false; errgrave = true;
+            }
+        }
+        public void sedeconnecter()
+        {
+            if (!connopen)
+                return;
+            try
+            {
+                myConnection.Close();
+                myConnection.Dispose();
+                connopen = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erreur fermeture bdd : " + err, "PBS deconnection", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+                errgrave = true;
+            }
         }
     }
 }
